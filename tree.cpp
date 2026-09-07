100a12d434fb292cc0c1e7eab28ab02550b5bc69
07yang153 <2952702757@qq.com>
2026-09-07 21:02:40 +0800

    并查集

--- tree.cpp
+++ tree.cpp
@@ -1,43 +1,43 @@
 #include <iostream>
-#include <string>
 using namespace std;
-const int N = 1e6;
-int son[N][26],cnt[N],idx;
-void insert(string s){
-    int p = 0;
-    for(char ch : s){
-      int u = ch - 'a' ;
-      if(!son[p][u])son[p][u] = ++idx;
-         p = son[p][u];
-         }
-       cnt[p] ++ ;
-
+const int N =1e5;
+int q[N];
+//返回x的祖宗节点
+int find(int x){
+  if(q[x] != x)q[x] = find(q[x]);
+  return q[x];
 }
-int query(string s){
-    int p = 0;
-    for(char ch :s){
-      int u= ch -'a';
-       if(!son[p][u])return 0;
-       p = son[p][u];
-    }
-    return cnt[p];
+//判断是否在同一个集合
+bool is(int a,int b){
+   return find(a) == find(b);
+}
+void merge(int a,int b){
+    if(is(a,b))return;
+    q[find(a)] = find(b);
 }
 int main(){
-   int n;
-   cin >> n;
-   while(n--){
-       char a;
-       cin >> a;
-       if(a == 'I'){
-        string s;
-        cin >> s;
-        insert(s);
-       }else{
-        string s;
-        cin >>s;
-       cout <<  query(s) << endl;
-       }
-   }
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+     int n,m;
+     cin >> n >>m;
+     for(int i= 1;i<=n;++i){
+       q[i] = i;
+    }
+    while(m--){
+       char p;
+       cin >> p;
+       int a,b;
+       if(p == 'M'){
+          cin >> a>>b;
+        merge(a,b);
+       }else {
+        cin >> a>>b;
+        if(is(a,b)){cout << "Yes\n";
+        }else{
+          cout << "No\n";
+         }
+    }
+    }
+    return 0;
 
-  return 0;
 }
